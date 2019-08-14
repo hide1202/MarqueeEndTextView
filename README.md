25de1a917e6ddfba0ab9abfec210648a4d00ebb8
Taegyeong Kim <hide1202@naver.com>
2019-08-14 14:40:37 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# MarqueeEndTextView
+- This project is textView for marquee that stop when text is located end of view.
+
+## Requirements
+- Min Android SDK : 21 (Lollipop)
---
+++ app/src/main/java/io/viewpoint/widget/samples/App.kt
@@ -0,0 +1,9 @@
+package io.viewpoint.widget.samples
+
+import android.app.Application
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
