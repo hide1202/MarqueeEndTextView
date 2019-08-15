b7ad7d6d297f8dc245cdb6d59b37f7646c0f3dc6
Taegyeong Kim <hide1202@naver.com>
2019-08-15 09:46:18 +0900

    Add RxJava. Support completable for animation.

--- app/src/main/java/io/viewpoint/widget/samples/MainActivity.kt
+++ app/src/main/java/io/viewpoint/widget/samples/MainActivity.kt
@@ -1,6 +1,7 @@
 package io.viewpoint.widget.samples
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import io.viewpoint.widget.marquee.startMarqueeAnimation
 import kotlinx.android.synthetic.main.activity_main.*
@@ -14,7 +15,9 @@ class MainActivity : AppCompatActivity() {
         startButton.setOnClickListener {
             marqueeTextView.startMarqueeAnimation(
                 delayMilliseconds = 500L
-            )
+            ).subscribe({
+                Toast.makeText(this, "Complete animation!", Toast.LENGTH_SHORT).show()
+            }, { /* do nothing*/ })
         }
     }
 }
--- library/src/main/java/io/viewpoint/widget/marquee/MarqueeAnimation.kt
+++ library/src/main/java/io/viewpoint/widget/marquee/MarqueeAnimation.kt
@@ -1,17 +1,44 @@
 package io.viewpoint.widget.marquee
 
+import android.content.Context
 import android.graphics.Rect
+import android.util.Log
+import android.view.animation.Interpolator
 import android.view.animation.LinearInterpolator
 import android.widget.Scroller
 import android.widget.TextView
+import io.reactivex.Completable
+import io.reactivex.subjects.CompletableSubject
 import java.util.concurrent.TimeUnit
+
+fun log(msg: String) {
+    Log.d("Marquee", msg)
+}
 
 class MarqueeAnimation(
     private val textView: TextView,
     private val delayMilliseconds: Long,
     private val durationMilliseconds: Int
 ) {
-    private val scroller: Scroller = Scroller(textView.context, LinearInterpolator())
+    private class CompletableScroller(
+        context: Context,
+        interpolator: Interpolator
+    ) : Scroller(context, interpolator) {
+        private val completeSignal = CompletableSubject.create()
+
+        override fun computeScrollOffset(): Boolean {
+            val result = super.computeScrollOffset()
+            log("computeScrollOffset : $isFinished, result : $result")
+            if (isFinished && duration > 0) {
+                completeSignal.onComplete()
+            }
+            return result
+        }
+
+        fun toCompletable(): Completable = completeSignal.hide()
+    }
+
+    private val scroller = CompletableScroller(textView.context, LinearInterpolator())
 
     init {
         textView.setScroller(scroller)
@@ -26,7 +53,8 @@ class MarqueeAnimation(
             return bounds.width()
         }
 
-    fun startAnimation() {
+    fun startAnimation(): Completable {
+        log("startAnimation")
         with(textView) {
             scroller.startScroll(0, 0, 0, 0, 0)
             invalidate()
@@ -45,15 +73,15 @@ class MarqueeAnimation(
                 invalidate()
             }, delayMilliseconds)
         }
+
+        return scroller.toCompletable()
     }
 }
 
 fun TextView.startMarqueeAnimation(
     delayMilliseconds: Long = 0L,
     durationMilliseconds: Int = TimeUnit.SECONDS.toMillis(1).toInt()
-) {
-    MarqueeAnimation(this, delayMilliseconds, durationMilliseconds)
-        .also {
-            it.startAnimation()
-        }
+): Completable {
+    return MarqueeAnimation(this, delayMilliseconds, durationMilliseconds)
+        .startAnimation()
 }
